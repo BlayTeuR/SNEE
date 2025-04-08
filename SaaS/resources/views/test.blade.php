54c74cd8847183c5198b287d3ac31609883aac9c
Bastien Jallais <bastien.jallais9@etu.univ-lorraine.fr>
2025-04-08 10:44:14 +0200

    [ARCHITECTURE] tailwin + laravel +vite
    automatisation de l'architecture sur la base du patron MVC

---
+++ SaaS/resources/views/test.blade.php
@@ -0,0 +1,14 @@
+<!-- resources/views/test.blade.php -->
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css') <!-- Inclusion du fichier CSS compilé par Vite -->
+    <title>Test Tailwind CSS</title>
+</head>
+<body class="bg-gray-100 p-10">
+<h1 class="text-4xl font-extrabold text-center text-blue-600">Hello, Laravel avec Tailwind et Vite !</h1>
+<p class="mt-4 text-center">Test du fonctionnement de Laravel + tailwind</p>
+</body>
+</html>
