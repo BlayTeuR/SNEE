8d5aa85fab75fd788faf3de760b93d88d1896dc3
Bastien Jallais <bastien.jallais9@etu.univ-lorraine.fr>
2025-04-09 09:34:41 +0200

    [BD] Model dépannage
    + insertion dans la BD des tables depannage, photos, historique

---
+++ SaaS/app/Models/Depannage.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Depannage extends Model
+{
+    protected $fillable = [
+        'nom', 'adresse', 'contact_email', 'statut', 'description_probleme',
+        'telephone', 'type_materiel', 'message_erreur', 'infos_supplementaires'
+    ];
+
+    public function historiques()
+    {
+        return $this->hasMany(Historique::class);
+    }
+
+    public function photos()
+    {
+        return $this->hasMany(Photo::class);
+    }
+}
---
+++ SaaS/app/Models/Photo.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Photo extends Model
+{
+    protected $fillable = ['depannage_id', 'chemin_photo'];
+
+    public function depannage(){
+        return $this->belongsTo(Depannage::class);
+    }
+}
