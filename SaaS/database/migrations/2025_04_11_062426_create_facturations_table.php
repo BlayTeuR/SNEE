994cc883d2d3e2dee7bfead3169f845de6b9f46f
Bastien Jallais <bastien.jallais9@etu.univ-lorraine.fr>
2025-04-11 08:28:22 +0200

    [BD] création table facturation

---
+++ SaaS/database/migrations/2025_04_11_062426_create_facturations_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('facturations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('depannage_id')->constrained()->onDelete('cascade');
+            $table->integer('montant')->comment('Montant de la facturation');
+            $table->string('statut')->default('Non payé')->comment('Statut de la facturation');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('facturations');
+    }
+};
