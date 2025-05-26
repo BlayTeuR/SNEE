eb1276b9b1b2323ce6360f98c20ead1ceb11f852
Bastien Jallais <bastien.jallais9@etu.univ-lorraine.fr>
2025-05-26 16:48:31 +0200

    [DEV] test nginx

--- SaaS/vite.config.js
+++ SaaS/vite.config.js
@@ -1,18 +1,32 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        hmr: {
-            host: 'https://639a-212-234-17-153.ngrok-free.app', // <-- IP de ton PC --- changer lorsque l'adresse IP local du pc change
-        }
-    },
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+export default defineConfig(({ command }) => {
+    if (command === 'serve') {
+        return {
+            server: {
+                host: '0.0.0.0',
+                port: 5173,
+                hmr: {
+                    host: 'https://639a-212-234-17-153.ngrok-free.app',
+                },
+            },
+            plugins: [
+                laravel({
+                    input: ['resources/css/app.css', 'resources/js/app.js'],
+                    refresh: true,
+                }),
+            ],
+        };
+    } else {
+        // build config prod (par défaut)
+        return {
+            plugins: [
+                laravel({
+                    input: ['resources/css/app.css', 'resources/js/app.js'],
+                    refresh: false,
+                }),
+            ],
+        };
+    }
 });
