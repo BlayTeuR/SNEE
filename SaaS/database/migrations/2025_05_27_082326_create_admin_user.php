c048674497af9a881bba8dabccb7ce42a4fd5534
Bastien Jallais <bastien.jallais9@etu.univ-lorraine.fr>
2025-05-27 08:27:49 +0200

    [BD] create default admin

---
+++ SaaS/database/migrations/2025_05_27_082326_create_admin_user.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'role' => 'admin',
+            'password' => Hash::make('password'), // ⚠️ change ce mot de passe
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::table('users')->where('email', 'admin@example.com')->delete();
+    }
+};
